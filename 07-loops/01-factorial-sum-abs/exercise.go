8cc3855ec0e485e79994d8e2650db3a539a88ab5
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-10 11:24:07 +0100

    solved 07-loops/01-factorial-sum-abs

--- 07-loops/01-factorial-sum-abs/exercise.go
+++ 07-loops/01-factorial-sum-abs/exercise.go
@@ -3,4 +3,9 @@ package factorial
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func calcAbs(n int) int {
+	if n >= 0 {
+		return n
+	}
+	return -1 * n
+}
