a1a185a5296c9ddcd284a2ba01b05a659db171d5
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-10 08:25:48 +0100

    Solved 02-numbers

--- 02-types/02-numbers/exercise.go
+++ 02-types/02-numbers/exercise.go
@@ -1,10 +1,25 @@
 package calculator
 
 import (
-	"fmt"
+	"math"
+	"strconv"
 )
 
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func gmean(x, y float64) int {
+	return int(math.Round(math.Sqrt(x * y)))
+}
+
+func gmeanString(x, y string) (int, error) {
+	var x_float, err_x = strconv.ParseFloat(x, 64)
+	if err_x != nil {
+		return 0, err_x
+	}
+	var y_float, err_y = strconv.ParseFloat(y, 64)
+	if err_y != nil {
+		return 0, err_y
+	}
+	return gmean(x_float, y_float), nil
+}
