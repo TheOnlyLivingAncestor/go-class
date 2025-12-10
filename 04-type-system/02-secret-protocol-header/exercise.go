67d5f028164b38ee3c385faed1a0ed2f0ecd79a8
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-10 11:01:48 +0100

    solved 02-secret-protocol-header

--- 04-type-system/02-secret-protocol-header/exercise.go
+++ 04-type-system/02-secret-protocol-header/exercise.go
@@ -5,5 +5,15 @@ package secretprotocolheader
 
 // createPublishFixHeader constructs an octet (8-bit long byte) based on its three arguments and the fix QoS setting.
 func createPublishFixHeader(isFirstAttempt bool, isBroadcasted bool, isSecure bool) byte {
-	// INSERT YOUR CODE HERE
+	header := byte(0b01001000)
+	if isFirstAttempt {
+		header |= 0b00010000
+	}
+	if isBroadcasted {
+		header |= 0b00000010
+	}
+	if isSecure {
+		header |= 0b00000001
+	}
+	return header
 }
