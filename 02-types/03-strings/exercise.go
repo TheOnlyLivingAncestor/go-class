96905c35650ab97593529730aae8b8928763b335
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-10 08:34:30 +0100

    solved 03-strings

--- 02-types/03-strings/exercise.go
+++ 02-types/03-strings/exercise.go
@@ -3,4 +3,48 @@ package strings
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func multilineString() string {
+	return `some
+multiline
+string`
+}
+
+func stringLen(s string) int {
+	return len(s)
+}
+
+func trimFirstChar(s string) string {
+	if stringLen(s) == 0 {
+		return s
+	}
+	return s[1:]
+}
+
+func trimLastChar(s string) string {
+	if stringLen(s) == 0 {
+		return s
+	}
+	return s[:stringLen(s)-1]
+}
+
+func swapFirstChar(s string) string {
+	if stringLen(s) == 0 {
+		return s
+	}
+	return string("A" + trimFirstChar(s))
+}
+
+func swapLastChar(s string) string {
+	if stringLen(s) == 0 {
+		return s
+	}
+	return string(trimLastChar(s) + "A")
+}
+
+func prependChar(s string) string {
+	return string("A" + s)
+}
+
+func appendChar(s string) string {
+	return string(s + "A")
+}
