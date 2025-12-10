979425f39f56e594177c47516f405fbc70f88527
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-10 11:47:37 +0100

    solved 07-loops/02-digits

--- 07-loops/02-digits/exercise.go
+++ 07-loops/02-digits/exercise.go
@@ -3,4 +3,11 @@ package digits
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func multiplyDigits(n int) int {
+	product := 1
+	for n > 0 {
+		product *= n % 10
+		n = n / 10
+	}
+	return product
+}
