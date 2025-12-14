9c97c8c14282b5203496235b83d7e736ca7d4c23
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 17:05:47 +0100

    solved 03-channel-broadcast

--- 21-channels/03-channel-broadcast/exercise.go
+++ 21-channels/03-channel-broadcast/exercise.go
@@ -1,6 +1,35 @@
 package channelbroadcaster
 
+import "context"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
+func channelBroadcast(ctx context.Context, input <-chan any, outputs []chan<- any) {
+
+	//The function should start its own goroutine
+	go func() {
+		defer func() {
+			for _, ch := range outputs {
+				close(ch)
+			}
+		}()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case v, ok := <-input:
+				if !ok {
+					return
+				}
+				for _, ch := range outputs {
+					ch <- v
+				}
+
+			}
+		}
+
+	}()
+}
