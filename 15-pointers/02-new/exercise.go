6751623862c2044ff2435c10cc0cb4dcd3b0e291
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 13:04:15 +0100

    solved 02-new

--- 15-pointers/02-new/exercise.go
+++ 15-pointers/02-new/exercise.go
@@ -2,3 +2,11 @@ package pointernew
 
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
+
+func newValue() *string {
+
+	return_value := new(string)
+	*return_value = "test"
+
+	return return_value
+}
