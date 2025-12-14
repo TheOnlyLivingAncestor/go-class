a52f6b3a998ceb27df1a7d42702b3d1ce50faa8f
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 18:32:51 +0100

    solved 02-functional

--- 24-generics/02-functional/exercise.go
+++ 24-generics/02-functional/exercise.go
@@ -3,4 +3,12 @@ package functional
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func filter[E any](input []E, predicate func(E) bool) []E {
+	var result []E
+	for _, elem := range input {
+		if predicate(elem) {
+			result = append(result, elem)
+		}
+	}
+	return result
+}
