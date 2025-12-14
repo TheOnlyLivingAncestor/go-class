8a6c70dc50edb84f4eb581a6055ed5cf3e456de1
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 18:29:30 +0100

    solved 01-sorting

--- 24-generics/01-sorting/exercise.go
+++ 24-generics/01-sorting/exercise.go
@@ -1,7 +1,21 @@
 package sorting
 
+import "sort"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
-func sortSlice(input []interface{}) []interface{} {}
+type Orderable interface {
+	~uint8 | ~int32
+}
+
+func sortSlice[T Orderable](s []T) []T {
+	result := make([]T, len(s))
+	copy(result, s)
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
+	})
+
+	return result
+}
