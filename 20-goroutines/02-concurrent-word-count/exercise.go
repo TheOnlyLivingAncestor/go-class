5d83bbe06c1f46d80e767a2c75a0edb4098c4905
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 15:05:30 +0100

    solved 02-concurrent-word-count

--- 20-goroutines/02-concurrent-word-count/exercise.go
+++ 20-goroutines/02-concurrent-word-count/exercise.go
@@ -1,6 +1,48 @@
 package wordcount
 
+import (
+	"runtime"
+	"strings"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func CountWords(texts []string) map[string]int {
+	workers := runtime.NumCPU()
+
+	jobs := make(chan string)
+	results := make(chan map[string]int)
+
+	//Workerek végzik a stringeken a számolást
+	for i := 0; i < workers; i++ {
+		go func() {
+			local := make(map[string]int)
+			for text := range jobs {
+				words := strings.Fields(text)
+				for _, w := range words {
+					local[w]++
+				}
+			}
+			results <- local
+		}()
+	}
+
+	//Producer csinálja a feldolgozandó szövegeket a slice-ból
+	go func() {
+		for _, t := range texts {
+			jobs <- t
+		}
+		close(jobs)
+	}()
+
+	final := make(map[string]int)
+	for i := 0; i < workers; i++ {
+		partial := <-results
+		for w, c := range partial {
+			final[w] += c
+		}
+	}
+
+	return final
+}
