4b244159425ba1abf8e2075077f8395d7e57d2c3
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 15:32:24 +0100

    solved 03-sleep-sort

--- 20-goroutines/03-sleep-sort/exercise.go
+++ 20-goroutines/03-sleep-sort/exercise.go
@@ -1,6 +1,61 @@
 package sleepSort
 
+import (
+	"runtime"
+	"sync"
+	"time"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
-// INSERT YOUR CODE HERE
+func reverseSleepSort(input []uint) []uint {
+	//kellenek workerek akiknek szétosztom az inputon lévő számokat
+	if len(input) == 0 {
+		return nil
+	}
+	workers := runtime.NumCPU()
+
+	//kell egy channel ahova bepotyogtatják a számokat
+	jobs := make(chan uint)
+
+	//végén össze kell szedni a számokat egy másik channelen
+	results := make(chan uint)
+
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			//megkapja a worker a számot és sleep-elni kell vele
+			for num := range jobs {
+				ms := 500 - int(num)*10
+				time.Sleep(time.Duration(ms) * time.Millisecond)
+				results <- num
+			}
+		}()
+	}
+
+	//Be kell potyogtatni a számokat a channelre
+	go func() {
+		for _, t := range input {
+			jobs <- t
+		}
+		close(jobs)
+	}()
+
+	//Bevárjuk a workereket
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	//és erről a második channelről kell kialakítani a visszatérési értéket
+	final := make([]uint, 0, len(input))
+	for v := range results {
+		final = append(final, v)
+	}
+
+	return final
+
+}
