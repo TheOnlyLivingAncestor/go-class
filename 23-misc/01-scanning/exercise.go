bce3ffc4c3df3577532f0d0de9a2bd0e38539267
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 18:08:54 +0100

    solved 01-scanning

--- 23-misc/01-scanning/exercise.go
+++ 23-misc/01-scanning/exercise.go
@@ -1,7 +1,9 @@
 package scanning
 
 import (
+	"bufio"
 	"io"
+	"unicode"
 )
 
 // DO NOT REMOVE THIS COMMENT
@@ -9,5 +11,16 @@ import (
 
 // counter reads a text and returns the counted values.
 func counter(reader io.Reader) int {
-	// INSERT YOUR CODE HERE
+	bufioReader := bufio.NewReader(reader)
+	var count int
+	for {
+		ch, _, err := bufioReader.ReadRune()
+		if err != nil {
+			break
+		}
+		if unicode.IsLower(ch) {
+			count++
+		}
+	}
+	return count
 }
