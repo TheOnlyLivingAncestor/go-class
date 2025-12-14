9acaa9c0d321ac1d4a80cba219b27d81feea4624
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 16:54:50 +0100

    solved 02-channel-multiplex

--- 21-channels/02-channel-multiplex/exercise.go
+++ 21-channels/02-channel-multiplex/exercise.go
@@ -1,6 +1,41 @@
 package channelmultiplexer
 
+import (
+	"context"
+	"sync"
+)
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // INSERT YOUR CODE HERE
+func channelMultiplex(ctx context.Context, inputs []chan any) chan any {
+	output := make(chan any)
+
+	var wg sync.WaitGroup
+
+	for _, input := range inputs {
+		wg.Add(1)
+		go func(ch chan any) {
+			defer wg.Done()
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case v, ok := <-ch:
+					if !ok {
+						return
+					}
+					output <- v
+				}
+			}
+		}(input)
+	}
+
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+
+	return output
+}
