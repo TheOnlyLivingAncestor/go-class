197aa19bf72bef35af33a9b68fa080efe6c5ae8f
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 14:00:53 +0100

    solved 03-struct-embedding

--- 19-structs/03-struct-embedding/exercise.go
+++ 19-structs/03-struct-embedding/exercise.go
@@ -1,5 +1,7 @@
 package structembedding
 
+import "encoding/json"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
@@ -7,21 +9,39 @@ package structembedding
 
 // Author represents information about the book's author
 type Author struct {
-	// TODO: Define the Author struct fields
+	Name, Address string
 }
 
 // Book represents information about a book
 type Book struct {
-	// TODO: Define the Book struct fields, embedding the Author struct
+	Author      Author
+	Title, ISBN string
+	Pages       int
 }
 
 // Article represents information about a article
 type Article struct {
-	// TODO: Define the Article struct fields, embedding the Author struct
+	Author         Author
+	Title, Journal string
+	Year           int
 }
 
 // ParseBook parses the given JSON data into a Book struct
-func ParseBook(jsonData []byte) (Book, error)
+func ParseBook(jsonData []byte) (Book, error) {
+	var b Book
+	err := json.Unmarshal(jsonData, &b)
+	if err != nil {
+		return Book{}, err
+	}
+	return b, nil
+}
 
 // ParseArticle parses the given JSON data into a Article struct
-func ParseArticle(jsonData []byte) (Article, error)
+func ParseArticle(jsonData []byte) (Article, error) {
+	var a Article
+	err := json.Unmarshal(jsonData, &a)
+	if err != nil {
+		return Article{}, err
+	}
+	return a, nil
+}
