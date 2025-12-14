5ae8097c3cfac2a95e55574b769f932ffea2f265
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 18:22:21 +0100

    solved 02-map-as-sets

--- 23-misc/02-map-as-sets/exercise.go
+++ 23-misc/02-map-as-sets/exercise.go
@@ -1,7 +1,10 @@
 package search
 
 import (
+	"bufio"
 	"io"
+	"regexp"
+	"strings"
 )
 
 // DO NOT REMOVE THIS COMMENT
@@ -9,5 +12,29 @@ import (
 
 // search reads a text and a word and returns true if the word appears in the text and false if it does not.
 func contain(reader io.Reader, word string) bool {
-	// INSERT YOUR CODE HERE
+	bufio_reader := bufio.NewReader(reader)
+
+	text, err := bufio_reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return false
+	}
+
+	re := regexp.MustCompile(`[^a-zA-Z0-9 ]+`)
+	cleaned_text := re.ReplaceAllString(text, "")
+	cleaned_text = strings.ToLower(cleaned_text)
+	words := strings.Fields(cleaned_text)
+
+	count := 0
+
+	for _, w := range words {
+		if w == "time" {
+			count++
+			if count > 1 {
+				return true
+			}
+		}
+	}
+
+	return false
+
 }
