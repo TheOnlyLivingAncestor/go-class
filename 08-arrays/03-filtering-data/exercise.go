d882f558f3f1e9fd65fed577973e99e5aacc1f30
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 12:03:48 +0100

    solved 03-filtering-data

--- 08-arrays/03-filtering-data/exercise.go
+++ 08-arrays/03-filtering-data/exercise.go
@@ -1,9 +1,24 @@
 package filteringdata
 
+import "fmt"
+
 // DO NOT REMOVE THIS COMMENT
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
 
 // filterData filters a slice based in an index slice.
 func filterData(keys []string, indices []int) [10]string {
-	// INSERT YOUR CODE HERE
+	filtered_array := [10]string{}
+
+	var array_index int
+	if len(keys) == len(indices) {
+		for i := 0; i < len(keys); i++ {
+			fmt.Println()
+			if indices[i] > 4 {
+				filtered_array[array_index] = keys[i]
+				array_index++
+			}
+		}
+	}
+
+	return filtered_array
 }
