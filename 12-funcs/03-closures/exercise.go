07a833b168ed75719d81cf6cab93fa2783ca53e4
TheOnlyLivingAncestor <vitez.alexandra@gmail.com>
2025-12-14 12:58:19 +0100

    solved 03-closures

--- 12-funcs/03-closures/exercise.go
+++ 12-funcs/03-closures/exercise.go
@@ -1,7 +1,20 @@
 package closures
 
+import "errors"
+
 // DO NOT REMOVE THIS COMMENT
+//
 //go:generate go run ../../exercises-cli.go -student-id=$STUDENT_ID generate
-func proxy(...){
-	// INSERT YOUR CODE HERE
+func proxy(f func(string) int) func(string) (int, error) {
+	on := true
+
+	return func(s string) (int, error) {
+		if on {
+			res := f(s)
+			on = !on
+			return res, nil
+		}
+		on = !on
+		return 0, errors.New("Error")
+	}
 }
